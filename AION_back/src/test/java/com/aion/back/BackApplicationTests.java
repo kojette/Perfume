210cb0d3ac1ce2dca65f11a6fd681aa5f48e0ccf
ksm <823001k@gmail.com>
2026-01-15 13:50:36 +0900

    스프링부트 초기 설정 완료

---
+++ AION_back/src/test/java/com/aion/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aion.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
