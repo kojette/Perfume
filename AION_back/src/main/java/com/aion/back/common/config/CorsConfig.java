a09dde93217dc991231e300d40fe9cf9acb44ba4
Rookie <rookie@Rookieui-MacBookAir.local>
2026-01-29 17:09:03 +0900

    API 접근 허용

--- AION_back/src/main/java/com/aion/back/common/config/CorsConfig.java
+++ AION_back/src/main/java/com/aion/back/common/config/CorsConfig.java
@@ -1,4 +1,22 @@
 package com.aion.back.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+// CorsConfig : 브라우저나 테스트 도구(Postman)의 접근 권한을 관리
 public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
+    }
 }
