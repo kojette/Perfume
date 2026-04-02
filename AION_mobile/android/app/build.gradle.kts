0fbd6706792ad8bcaa5e95f85dfaca6bf187311a
kojette <dear.adore5@gmail.com>
2026-04-03 02:41:31 +0900

    README 및 프로젝트 구조 업데이트

--- AION_mobile/android/app/build.gradle.kts
+++ AION_mobile/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 android {
     namespace = "com.aion.perfume"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
